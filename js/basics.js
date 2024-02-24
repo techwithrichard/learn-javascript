816f0dc137640a823ae22addd7825946f04a3b87
techwithrichard <richardkoech69@gmail.com>
2024-02-24 11:19:08 +0300

    project setup

---
+++ js/basics.js
@@ -0,0 +1,22 @@
+// variables
+// data types
+// operators
+// functions
+// loops
+// conditionals\\
+// 1. variables
+// var, let, const
+let name="learning javascript"
+const PI = 3.142; //cont values cannot be changed
+let age = 25;
+let isMarried = false;
+let person = {
+    name: "John",
+    age: 30,
+    isMarried: false,
+
+};
+
+console.log(name, PI, age, isMarried, person);
+
+
